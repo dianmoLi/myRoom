2281271481785e7d22df6ceeec2ceb3ba0c8493e
MissDianmo <lidianmo@qq.com>
2016-11-17 16:20:53 +0800

    click
    input click enter

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
+function toSearch(){
+	window.location.href="searchW.html?searchName="+$("#searchName").val();
+}
 $(document).ready(function(){
 	$("#startButton").click(function(){
 		window.location.href="searchW.html?searchName="+$("#searchName").val();
+	});
+	$("#searchName").keydown(function(e){
+		if(e.keyCode == 13) {
+	       toSearch();
+	    } 
 	});
 });
