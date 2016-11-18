87e55df80744e65119593f4581cd11631b94d805
MissDianmo <lidianmo@qq.com>
2016-11-18 14:25:10 +0800

    update  a

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 function toSearch(){
-	window.location.href="searchW.html?searchName="+$("#searchName").val();
+	window.location.href="searching.html?searchName="+$("#searchName").val();
 }
 $(document).ready(function(){
 	$("#startButton").click(function(){
-		window.location.href="searchW.html?searchName="+$("#searchName").val();
+		window.location.href="searching.html?searchName="+$("#searchName").val();
 	});
 	$("#searchName").keydown(function(e){
 		if(e.keyCode == 13) {
